bbc07841dfcd8796eb9f4d949467bf0dd0908bc5
D4n13l3Q <daniele.quasimodo@edu.itspiemonte.it>
2022-12-31 17:46:30 +0100

    Modificato api perché usi GET, PUT, DELETE, ecc invece che solo POST (più varii fix)

--- books/index.js
+++ books/index.js
@@ -9,33 +9,134 @@ const PORT = process.env.PORT || 3001;
 // If the server recevies JSON in a request's body it will automatically convert the JSON to a JavaScript object.
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Libreria Alfonso aperta');
+
+app.get('/', (request, response) => {
+    response.send('Libreria Alfonso aperta');
 });
 
-app.post('/add', (req, res) => {
-    res.json(db.addBook(req.body));
-});
 
-app.post('/update', (req, res) => {
-    res.json(db.updateBook(req.body));
-});
-
-app.post('/delete', (req, res) => {
-    res.json(db.removeBook(req.body.id));
-});
-
-app.post('/getAll', (req, res) => {
-    res.json(db.getAllBooks());
-});
-
-app.post('/getId', (req, res) => {
-    res.json(db.getBookById(req.body.id));
+app.get('/api/books/:id', (request, response) => {
+    try {
+		const book = db.getBook(request.params.id);
+		if (book) {
+			return response.status(200).json({
+				status: 'success',
+				data: book,
+			});
+		} else {
+			return response.status(404).json({
+				status: 'fail',
+				error: 'Not found',
+			});
+		}
+	} catch (err) {
+		return response.status(500).json({
+			status: 'fail',
+			error: err.toString(),
+		});
+	}
 })
 
-app.post('/setAvailable', (req, res) => {
-    res.json(db.setAvailable(req.body.id));
-})
+
+app.get('/api/books', (request, response) => {
+	const pageNumber = request.query.page ?? 0;
+	//const pageSize = request.query.pageSize;
+	const pageSize = 10;
+	
+	try {
+		const books = db.getAllBooks(pageNumber, pageSize);
+		return response.status(200).json({
+			status: 'success',
+			page: pageNumber,
+			data: books
+		});
+	} catch (err) {
+		return response.status(500).json({
+			status: 'fail',
+			error: err.toString(),
+		});
+	}
+});
+
+
+app.delete('/api/books/:id', (request, response) => {
+    try {
+		result = db.removeBook(request.params.id);
+		if (result) {
+			return response.status(204).json({
+				status: 'success'
+			});
+		} else {
+			return response.status(404).json({
+				status: 'fail',
+				error: 'Not found',
+			});
+		}
+	} catch (err) {
+		return response.status(500).json({
+			status: 'fail',
+			error: err.toString(),
+		});
+	}
+});
+
+
+app.put('/api/books/:id', (request, response) => {
+    const bookData = request.body;
+	const bookId = request.params.id;
+	
+	try {
+		const updatedBook = db.updateBook(bookData, bookId);
+		if (updatedBook) {
+			return response.status(200).json({
+				status: 'success',
+				data: updatedBook,
+			});
+		} else {
+			return response.status(404).json({
+				status: 'fail',
+				error: 'Not found',
+			});
+		}
+	} catch (err) {
+		return response.status(500).json({
+			status: 'fail',
+			error: err.toString(),
+		});
+	}
+});
+
+
+app.post('/api/books', (request, response) => {
+    const bookData = request.body;
+	
+	try {
+		const newBook = db.updateBook(bookData);
+		return response.status(201).json({
+			status: 'success',
+			data: newBook,
+		});
+	} catch (err) {
+		return response.status(500).json({
+			status: 'fail',
+			error: err.toString(),
+		});
+	}
+});
+
+
+
+// Default route for bad requests
+app.use(
+  //'/',
+  function (request, reply) {
+    reply.status(400).json({
+      success: false,
+      error: 'Bad request',
+    });
+  },
+);
+
 
 // Tell express to listen to communication on the specified port after the configuration is done.
 app.listen(PORT, () => console.log(`Libreria Alfonso sulla porta ${PORT}`));
