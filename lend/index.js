4b2981b936ad932d4c0762f5471493dfd07fb4c1
Rdx <100215987+Rdxv@users.noreply.github.com>
2023-01-01 20:16:36 +0100

    changed post methods to get, put and delete

--- lend/index.js
+++ lend/index.js
@@ -1,58 +1,181 @@
+// Import and config dotenv
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 // Import the Fake DB functions and the express Framework
-const db = require('./fakeData');
-const express = require('express');
+import * as db from './database/fake-db.js';
+import express from 'express';
+
 // Create the Server Instance
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT ?? 3002;
 
 // Tell express to use its JSON middleware.
-// If the server recevies JSON in a request's body it will automatically convert the JSON to a JavaScript object.
+// If the server receives JSON in a request's body it will automatically convert the JSON to a JavaScript object.
 app.use(express.json());
 
-/*
- Configure the REST paths for the server.
- Express provides functions to add paths. app.get() will listen for GET requests, app.post() for POST requests.
- These functions require the url part to listen to and a callback function to execute when something arrives at this url.
- The callback function to execute receives the request (req) and a JavaScript Object containing multiple reply functions (res) from express.
- At the end of our callback we use the reply functions to fulfill a request.
+
+app.get('/', (request, response) => {
+    response.send('Libreria Alfonso, servizio prestiti');
+});
+
+app.get('/api/lends/:lendId', (request, response) => {
+    try {
+        const lend = db.getLendById(request.params.id);
+        if (lend) {
+            return response.status(200).json({
+                status: 'success',
+                data: lend,
+            });
+        } else {
+            return response.status(404).json({
+                status: 'fail',
+                error: 'Not found',
+            });
+        }
+    } catch (err) {
+        return response.status(500).json({
+            status: 'fail',
+            error: err.toString(),
+        });
+    }
+})
+
+app.get('/api/lends/:customerId', (request, response) => {
+    try {
+        const lend = db.getLendByCustomer(request.params.id);
+        if (lend) {
+            return response.status(200).json({
+                status: 'success',
+                data: lend,
+            });
+        } else {
+            return response.status(404).json({
+                status: 'fail',
+                error: 'Not found',
+            });
+        }
+    } catch (err) {
+        return response.status(500).json({
+            status: 'fail',
+            error: err.toString(),
+        });
+    }
+})
+
+app.get('/api/lends/:bookId', (request, response) => {
+    try {
+        const lend = db.getLendByBook(request.params.id);
+        if (lend) {
+            return response.status(200).json({
+                status: 'success',
+                data: lend,
+            });
+        } else {
+            return response.status(404).json({
+                status: 'fail',
+                error: 'Not found',
+            });
+        }
+    } catch (err) {
+        return response.status(500).json({
+            status: 'fail',
+            error: err.toString(),
+        });
+    }
+})
+
+app.get('/api/lends', (request, response) => {
+    const pageNumber = request.query.page ?? 0;
+    //const pageSize = request.query.pageSize;
+    const pageSize = 10;
+
+    try {
+        const lends = db.getAllLends(pageNumber, pageSize);
+        return response.status(200).json({
+            status: 'success',
+            page: pageNumber,
+            data: lends
+        });
+    } catch (err) {
+        return response.status(500).json({
+            status: 'fail',
+            error: err.toString(),
+        });
+    }
+});
+
+app.delete('/api/lends/:lendId', (request, response) => {
+    try {
+        const result = db.removeLend(request.params.id);
+        if (result) {
+            return response.status(200).json({
+                status: 'success',
+                msg: 'Deleted'
+            });
+        } else {
+            return response.status(404).json({
+                status: 'fail',
+                error: 'Not found',
+            });
+        }
+    } catch (err) {
+        return response.status(500).json({
+            status: 'fail',
+            error: err.toString(),
+        });
+    }
+});
+
+app.put('/api/lends/:lendId', (request, response) => {
+    const lendData = request.body;
+    const lendId = request.params.id;
+
+    try {
+        const updatedLend = db.updateLend(lendData, lendId);
+        if (updatedLend) {
+            return response.status(200).json({
+                status: 'success',
+                data: updatedLend,
+            });
+        } else {
+            return response.status(404).json({
+                status: 'fail',
+                error: 'Not found',
+            });
+        }
+    } catch (err) {
+        return response.status(500).json({
+            status: 'fail',
+            error: err.toString(),
+        });
+    }
+});
+
+app.post('/api/lends', (request, response) => {
+    const lendData = request.body;
+
+    try {
+        const newLend = db.addLend(lendData);
+        return response.status(201).json({
+            status: 'success',
+            data: newLend,
+        });
+    } catch (err) {
+        return response.status(500).json({
+            status: 'fail',
+            error: err.toString(),
+        });
+    }
+});
+
+
+/* TODO: change this method
+
+app.post('/api/lends/return', (req, res) => {
+    res.json(db.returnLend(req.body.lendId, req.body.bookId));
+})
 */
 
-app.get('/', (req, res) => {
-    res.send('Ready to lend books.');
-});
-
-app.post('/add', (req, res) => {
-    res.json(db.addLend(req.body));
-});
-
-app.post('/update', (req, res) => {
-    res.json(db.updateLend(req.body));
-});
-
-app.post('/delete', (req, res) => {
-    res.json(db.removeLend(req.body.id));
-});
-
-app.post('/return', (req, res) => {
-    res.json(db.returnBook(req.body.lendId, req.body.bookId));
-})
-
-app.post('/getAll', (req, res) => {
-    res.json(db.getAllLends());
-});
-
-app.post('/getId', (req, res) => {
-    res.json(db.getLendById(req.body.id));
-})
-
-app.post('/getByBook', (req, res) => {
-    res.json(db.getLendByBook(req.body.bookId));
-})
-
-app.post('/getByCustomer', (req, res) => {
-    res.json(db.getLendByCustomer(req.body.customerId));
-})
-
 // Tell express to listen to communication on the specified port after the configuration is done.
-const port = process.env.PORT
-app.listen(port, () => console.log(`Lend Service listening on ${port}`));
+app.listen(PORT, () => console.log(`Lend Service listening on ${PORT}`));
