17e3da42eeb93eae8fea3d81eba8a2f0e10b919b
D4n13l3Q <daniele.quasimodo@edu.itspiemonte.it>
2023-01-16 02:14:21 +0100

    Updated schema and added resolvers and connectors for lends and customers APIs

--- graphql-gateway/graphql_logic.py
+++ graphql-gateway/graphql_logic.py
@@ -1,4 +1,4 @@
-from ariadne import QueryType, MutationType, make_executable_schema, load_schema_from_path, format_error
+from ariadne import ObjectType, QueryType, MutationType, make_executable_schema, load_schema_from_path, format_error
 
 import connectors
 
@@ -17,7 +17,7 @@ query = QueryType()
 mutation = MutationType()
 
 
-## Define resolversve
+## Books resolvers
 @query.field('book')
 async def resolve_book(*_, id):
     return await connectors.get_book_by_id(id)
@@ -39,7 +39,75 @@ async def resolve_update_book(_, info, id, input):
 async def resolve_delete_book(_, info, id):
     return await connectors.delete_book(id)
 
+book = ObjectType('Book')
+
+@book.field('lends')
+async def resolve_field_lends_in_book(obj, info):
+    return await connectors.get_lends({'bookId': obj['id']})
+
+
+
+## Lends resolvers
+@query.field('lend')
+async def resolve_lend(*_, id):
+    return await connectors.get_lend_by_id(id)
+
+@query.field('lends')
+async def resolve_lends(*_, **arguments):
+    return await connectors.get_lends(arguments)
+
+@mutation.field('addLend')
+async def resolve_add_lend(_, info, input):
+    return await connectors.add_lend(input)
+
+@mutation.field('updateLend')
+async def resolve_update_lend(_, info, id, input):
+    return await connectors.update_lend(id, input)
+
+@mutation.field('deleteLend')
+async def resolve_delete_lend(_, info, id):
+    return await connectors.delete_lend(id)
+
+lend = ObjectType('Lend')
+
+@lend.field('book')
+async def resolve_field_book(obj, info):
+    return await connectors.get_book_by_id(str(obj['bookId']))
+
+@lend.field('customer')
+async def resolve_field_customer(obj, info):
+    return await connectors.get_customer_by_id(obj['customerId'])
+
+
+
+## Customers resolvers
+@query.field('customer')
+async def resolve_customer(*_, id):
+    return await connectors.get_customer_by_id(id)
+
+@query.field('customers')
+async def resolve_customers(*_, **arguments):
+    return await connectors.get_customers(arguments)
+
+@mutation.field('addCustomer')
+async def resolve_add_customer(_, info, input):
+    return await connectors.add_customer(input)
+
+@mutation.field('updateCustomer')
+async def resolve_update_customer(_, info, id, input):
+    return await connectors.update_customer(id, input)
+
+@mutation.field('deleteCustomer')
+async def resolve_delete_customer(_, info, id):
+    return await connectors.delete_customer(id)
+
+customer = ObjectType('Customer')
+
+@customer.field('lends')
+async def resolve_field_lends_in_customer(obj, info):
+    return await connectors.get_lends({'customerId': obj['id']})
+
 
 
 ## Generate executable schema
-executable_schema = make_executable_schema(schema, [query, mutation] + custom_scalars)
+executable_schema = make_executable_schema(schema, [query, mutation, book, lend, customer] + custom_scalars)
