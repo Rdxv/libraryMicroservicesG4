0656f549f28d8374d3aa1af683cdacaab3402406
Alberto-finito <alberto.alineri@gmail.com>
2023-01-21 18:35:38 +0100

    Update index.js app1

--- notification/Kafka/app1/index.js
+++ notification/Kafka/app1/index.js
@@ -1,7 +1,37 @@
 const express = require('express')
+const kafka = require('kafka-node')
+const { STRING } = require('sequelize')
 const app = express()
-app.use(express.json())
-app.post('/',(req,res)=>{
+const sequelize = require('sequelize')
+app.use(express.json)
+
+const dbsAreRunning = () =>{
+const db = new sequelize(process.env.POSTGRES_URL)
+const User = db.define('user', {
+    name: sequelize.STRING,
+    email: sequelize.STRING,
+    password: sequelize.STRING
+})
+
+const client = new kafka.KafkaClient({kafkaHost: process.env.KKAFKA_BOOTSTRAP_SERVERS})
+const producer = new kafka.Producer(client)
+producer.on('ready', ()=> {
+    app.post('/',(req,res)=>{
+        producer.send([{topic: process.env.KAFKA_TOPIC,
+            messages: JSON.stringify(req.body)}],
+            (err, data)=> {
+            if(err) console.log(err)
+            else {
+                await User.create(req.body)
+                res.send(req.body)
+            }
+
+        })
+    
+    })
 
 })
+}
+setTimeout(dbsAreRunning, 10000)
+
 app.listen(process.env.PORT)
