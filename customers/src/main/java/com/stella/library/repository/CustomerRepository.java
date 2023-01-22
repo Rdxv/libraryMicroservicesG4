d66face00d916a7c245a4d958072e2fc5f1dcb7a
D4n13l3Q <daniele.quasimodo@edu.itspiemonte.it>
2023-01-22 15:11:56 +0100

    Fixed db connection and added dockerfile (and docker-compose for test)

--- customers/src/main/java/com/stella/library/repository/CustomerRepository.java
+++ customers/src/main/java/com/stella/library/repository/CustomerRepository.java
@@ -1,13 +1,13 @@
 package com.stella.library.repository;
 
-//import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.stella.library.model.Customer;
 
 import java.util.List;
 
-//@Repository
+@Repository
 public interface CustomerRepository extends MongoRepository<Customer, String> {
     
     List<Customer> findByNameContaining(String name);
