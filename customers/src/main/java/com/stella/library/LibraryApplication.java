39ff4264c274d5d1ce7a22f841b7e4720b10b32b
D4n13l3Q <daniele.quasimodo@edu.itspiemonte.it>
2023-01-22 21:51:33 +0100

    Added correlation id to logging

--- customers/src/main/java/com/stella/library/LibraryApplication.java
+++ customers/src/main/java/com/stella/library/LibraryApplication.java
@@ -3,11 +3,20 @@ package com.stella.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
+import com.stella.library.filter.CorrelationIdFilter;
 import com.stella.library.listener.GenerateUUIDListener;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+import jakarta.servlet.http.HttpFilter;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LibraryApplication {
+
+	@Bean
+    public HttpFilter securityFilter() {
+        return new CorrelationIdFilter();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(LibraryApplication.class);
