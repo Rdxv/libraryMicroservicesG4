1a404e131df9c223ec0dec52d87ed3a81d81a53b
D4n13l3Q <daniele.quasimodo@edu.itspiemonte.it>
2023-01-22 20:29:58 +0100

    Now the microservice uses standard UUIDv1 as id instead of Mongo IDs

--- customers/src/main/java/com/stella/library/LibraryApplication.java
+++ customers/src/main/java/com/stella/library/LibraryApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import com.stella.library.listener.GenerateUUIDListener;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(LibraryApplication.class);
+		springApplication.addListeners(new GenerateUUIDListener());
+		springApplication.run(args);
 	}
 
 }
